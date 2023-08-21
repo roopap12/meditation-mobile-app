fe292d543afa06e5613cbc2211e58838029c93ff
Roopa Patel <roopa12_patel@yahoo.co.nz>
2023-08-21 12:33:24 +1200

    style for meditation image

--- src/Components/Meditation.jsx
+++ src/Components/Meditation.jsx
@@ -3,7 +3,8 @@ import React from "react";
 function Meditation() {
   return (
     <div className="Meditation">
-      <img src="/Meditation.jpg" alt="Meditation" />
+      <img src="/Meditation.jpg" alt="Meditation" className="meditationImage" />
+      
       <p> Awake the world, one mind at a time!</p>
     </div>
   );
