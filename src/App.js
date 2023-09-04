448a03f0f76958fddec13ae6bd77b40a4b53b243
Roopa Patel <roopa12_patel@yahoo.co.nz>
2023-09-05 09:38:34 +1200

    Fixedd error on App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import Stopwatch from "./Components/Stopwatch";
 import Header from "./Components/Header";
 import Meditation from "./Components/Meditation";
-import { useLocation } from "react-router-dom";  // Extract meditation type from the location
+import { useLocation } from "react-router-dom"; // Extract meditation type from the location
 
 function App({ children }) {
   const [currentTab, setCurrentTab] = useState("original");
   // const [hasStarted, setHasStarted] = useState(false);
-  const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
+  const [currentTime, setCurrentTime] = useState(
+    new Date().toLocaleTimeString()
+  );
 
   // Get the current location to determine the meditation type
   const location = useLocation();
-  const params = new URLSearchParams(location.search);
-  const [selectedMeditation, setSelectedMeditation] = useState(params.get("type") || "morning");
+  const params = useMemo(() => {
+    return new URLSearchParams(location.search);
+  }, [location.search]);
+  // const params = new URLSearchParams(location.search);
+  const [selectedMeditation, setSelectedMeditation] = useState(
+    params.get("type") || "morning"
+  );
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -33,8 +40,10 @@ function App({ children }) {
     <div className="parent">
       <Header />
       <div className="child">
-
-        <Navbar setCurrentTab={setCurrentTab} setSelectedMeditation={setSelectedMeditation} />
+        <Navbar
+          setCurrentTab={setCurrentTab}
+          setSelectedMeditation={setSelectedMeditation}
+        />
       </div>
       <div className="main">
         <div className="button-box">
@@ -45,7 +54,9 @@ function App({ children }) {
           </div>
         </div>
         <div className="child content">
-          {currentTab === "meditation" && <Meditation selectedMeditation={selectedMeditation} />}
+          {currentTab === "meditation" && (
+            <Meditation selectedMeditation={selectedMeditation} />
+          )}
           {children}
         </div>
       </div>
